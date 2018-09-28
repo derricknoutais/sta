495811f476d6e52edd084654cce8e4988b274f3b
Derrick Noutais <derricknoutais@gmail.com>
2018-09-28 14:59:11 +0100

    Fixed Routings

--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,67 @@
 <?php
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::prefix('/fiche-renseignement')->group(function(){
+
+        Route::get('/', 'FicheRenseignementController@répertoire');
+        Route::view('/renseigner', 'fiche-renseignement.renseigner');
+        Route::prefix('/api')->group(function(){
+            Route::get('all', 'FicheRenseignementController@all');
+            Route::post('/enregistrer', 'FicheRenseignementController@enregistrer');
+        });
+        Route::prefix('/marque')->group(function(){
+            Route::view('créer', 'fiche-renseignement.marque.créer');
+            Route::prefix('/api')->group(function(){
+                Route::get('/all', 'MarqueController@all');
+                Route::post('/enregistrer', 'MarqueController@enregistrer');
+            });
+        });
+        Route::prefix('/type')->group(function(){
+            Route::get('/de-marque/{marque}', 'TypeController@chercheModèle');
+            Route::prefix('/api')->group(function(){
+                Route::get('/all', 'TypeController@all');
+                Route::post('/enregistrer', 'TypeController@enregistrer');
+            });
+        });
+        Route::prefix('/modèle')->group(function(){
+            Route::get('/de-type/{type}', 'ModèleController@chercheModèle');
+            Route::prefix('/api')->group(function(){
+                Route::get('/all', 'ModèleController@all');
+                Route::post('/enregistrer', 'ModèleController@enregistrer');
+            });
+        });
+        Route::prefix('/type-moteur')->group(function(){
+            Route::prefix('/api')->group(function(){
+                Route::post('/enregistrer', 'TypeMoteurController@enregistrer');
+            });
+        });
+        Route::prefix('/moteur')->group(function(){
+            Route::get('de-type/{type}', 'MoteurController@chercheMoteurs')->where('modele', '[0-9]+');
+            Route::prefix('/api')->group(function(){
+                Route::get('/all', 'MoteurController@all');
+                Route::post('/enregistrer', 'MoteurController@enregistrer');
+            });
+        });
+        Route::prefix('/modèle-type')->group(function(){
+            Route::prefix('/api')->group(function(){
+                // Route::get('/all', 'MoteurController@all');
+                Route::post('/enregistrer', 'ModèleTypeController@enregistrer');
+            });
+        });
+
+
+
+    });
 });
 
-Route::prefix('/fiche-renseignement')->group(function(){
 
-    Route::get('/', 'FicheRenseignementController@répertoire');
-    Route::view('/renseigner', 'fiche-renseignement.renseigner');
-    Route::prefix('/api')->group(function(){
-        Route::get('all', 'FicheRenseignementController@all');
-        Route::post('/enregistrer', 'FicheRenseignementController@enregistrer');
-    });
-    Route::prefix('/marque')->group(function(){
-        Route::view('créer', 'fiche-renseignement.marque.créer');
-        Route::prefix('/api')->group(function(){
-            Route::get('/all', 'MarqueController@all');
-            Route::post('/enregistrer', 'MarqueController@enregistrer');
-        });
-    });
-    Route::prefix('/type')->group(function(){
-        Route::get('/de-marque/{marque}', 'TypeController@chercheModèle');
-        Route::prefix('/api')->group(function(){
-            Route::get('/all', 'TypeController@all');
-            Route::post('/enregistrer', 'TypeController@enregistrer');
-        });
-    });
-    Route::prefix('/modèle')->group(function(){
-        Route::get('/de-type/{type}', 'ModèleController@chercheModèle');
-        Route::prefix('/api')->group(function(){
-            Route::get('/all', 'ModèleController@all');
-            Route::post('/enregistrer', 'ModèleController@enregistrer');
-        });
-    });
-    Route::prefix('/type-moteur')->group(function(){
-        Route::prefix('/api')->group(function(){
-            Route::post('/enregistrer', 'TypeMoteurController@enregistrer');
-        });
-    });
-    Route::prefix('/moteur')->group(function(){
-        Route::get('de-type/{type}', 'MoteurController@chercheMoteurs')->where('modele', '[0-9]+');
-        Route::prefix('/api')->group(function(){
-            Route::get('/all', 'MoteurController@all');
-            Route::post('/enregistrer', 'MoteurController@enregistrer');
-        });
-    });
-    Route::prefix('/modèle-type')->group(function(){
-        Route::prefix('/api')->group(function(){
-            // Route::get('/all', 'MoteurController@all');
-            Route::post('/enregistrer', 'ModèleTypeController@enregistrer');
-        });
-    });
-
-    
-    
-});
 
 
