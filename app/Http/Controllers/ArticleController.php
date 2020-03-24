01b13b50f0a16787fbd655ef8b043962143905e5
Derrick Noutais <derricknoutais@gmail.com>
2020-03-24 09:34:52 +0100

    nonCommande - commande

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -16,7 +16,7 @@ class ArticleController extends Controller
     }
 
     public function nonCommandé(){
-        return Article::where('état', 'enregistré')->with('fiche_renseignement', 'fiche_renseignement.marque', 'fiche_renseignement.type', 'fiche_renseignement.moteur')->get();
+        return Article::with('fiche_renseignement', 'fiche_renseignement.marque', 'fiche_renseignement.type', 'fiche_renseignement.moteur')->get();
     }
     public function changerEtat(Request $request, Article $article){
         $article->changeEtat($request->etat);
